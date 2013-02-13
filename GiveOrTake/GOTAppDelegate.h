9941383f31cd8e54716537083f6c79184aeb54b7
Nora Mullaney <nora@Noras-MacBook-Pro.local>
2013-02-13 13:15:33 -0800

    Initial Commit

---
+++ GiveOrTake/GOTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GOTAppDelegate.h
+//  GiveOrTake
+//
+//  Created by Nora Mullaney on 2/13/13.
+//  Copyright (c) 2013 Nora Mullaney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GOTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GiveOrTake/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GiveOrTake
+//
+//  Created by Nora Mullaney on 2/13/13.
+//  Copyright (c) 2013 Nora Mullaney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GOTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GOTAppDelegate class]));
+    }
+}
